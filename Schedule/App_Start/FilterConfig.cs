31c9fedcbd1f36e2acbaa9c549e4cb3889c50a9f
duy <duy>
2019-11-22 16:44:41 +0700

    upload client app

--- Schedule/App_Start/FilterConfig.cs
+++ Schedule/App_Start/FilterConfig.cs
@@ -7,6 +7,7 @@ namespace Schedule
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
+            filters.Add(new Cors());
             filters.Add(new HandleErrorAttribute());
         }
     }
